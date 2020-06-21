98cab7e9a7bf8c765728269a7826af35a72fe6dc
nick <nicholas.j.brooking@gmail.com>
2020-06-21 12:19:43 +0200

    allow all origins on controller

--- src/main/kotlin/net/dummyvariables/games/schach/server/game/WebSocketConfig.kt
+++ src/main/kotlin/net/dummyvariables/games/schach/server/game/WebSocketConfig.kt
@@ -1,14 +1,10 @@
 package net.dummyvariables.games.schach.server.game
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.socket.WebSocketHandler
-import org.springframework.web.socket.config.annotation.*
-import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler
-import java.net.http.WebSocket
-import java.util.logging.SocketHandler
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocketMessageBroker
--- src/main/kotlin/net/dummyvariables/games/schach/server/game/WebSocketController.kt
+++ src/main/kotlin/net/dummyvariables/games/schach/server/game/WebSocketController.kt
@@ -9,8 +9,10 @@ import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @Controller
+@CrossOrigin
 class WebSocketController(
         private val simpMessagingTemplate: SimpMessagingTemplate,
         private val messageService: MessageService,
