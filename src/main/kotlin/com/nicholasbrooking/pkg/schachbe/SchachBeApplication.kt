42f2642d0ac67a35d7ea8607a3e78e748bd6011e
nick <nicholas.j.brooking@gmail.com>
2020-09-30 11:15:36 +0200

    initializr

---
+++ src/main/kotlin/com/nicholasbrooking/pkg/schachbe/SchachBeApplication.kt
@@ -0,0 +1,11 @@
+package com.nicholasbrooking.pkg.schachbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchachBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchachBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/nicholasbrooking/pkg/schachbe/SchachBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nicholasbrooking.pkg.schachbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchachBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
